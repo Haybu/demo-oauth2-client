64dfc7d1dfd58610d75be559e6bb9c46ca471e47
Haytham Mohamed <hmohamed@Haythams-MBP-2.attlocal.net>
2018-05-08 14:35:48 -0500

    init

---
+++ src/main/java/io/agilehandy/demoauth2client/DemoAuth2ClientApplication.java
@@ -0,0 +1,12 @@
+package io.agilehandy.demoauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuth2ClientApplication.class, args);
+	}
+}
